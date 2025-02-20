b51a2e24f9dfb0ba54092415bb5253bf404ea2ef
MuradRashidov <muradrashidov995@gmail.com>
2025-02-20 16:27:30 +0400

    res

--- apps/paragraphui/src/app/api/auth/google/callback/route.ts
+++ apps/paragraphui/src/app/api/auth/google/callback/route.ts
@@ -3,7 +3,7 @@ import { createSession } from "@/lib/session";
 import { redirect } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const userId = searchParams.get("userId");
   const avatar = searchParams.get("avatar");
