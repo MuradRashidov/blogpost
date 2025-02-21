b553845c0fe2ee219464dc699fff2fdb3b446208
MuradRashidov <muradrashidov995@gmail.com>
2025-02-21 10:35:24 +0400

    url

--- apps/paragraphui/src/lib/constants.ts
+++ apps/paragraphui/src/lib/constants.ts
@@ -1,2 +1,3 @@
-export const API_URL = "http://localhost:8000/";
+//export const API_URL = "http://localhost:8000/";
+export const API_URL = "https://paragraphapi.onrender.com/";
 export const DEFAULT_PAGE_SIZE = 3;
